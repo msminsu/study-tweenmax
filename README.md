4fb8753b3300487d97ae6e2d619d4dc998a04aad
An-gun <msminsu1@ncsoft.com>
2018-08-20 08:42:08 +0900

    add

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tween Max 
+
+## Frater
+- 설정
+- 사용법
+- 메서드
+
+![map](./src/map.png)
---
+++ step01/ani.js
@@ -0,0 +1,5 @@
+(function($){
+    var header = $('#header');
+    TweenMax.to(header, 1, {opacity: 0, y:40});
+    
+})(jQuery);
